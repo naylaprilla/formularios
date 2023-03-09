31f2167b4563fc1e5a023d74e25e839c7fb6705d
naylaprilla <nayla.priscila@gmail.com>
2023-03-09 10:08:24 -0300

    add função validaCPF e checaCPFRepetido

--- js/validacao.js
+++ js/validacao.js
@@ -7,10 +7,10 @@ export function valida(input) {
 
     if (input.validity.valid) {
         input.parentElement.classList.remove('input-container--invalido')
-        input.parentElement.querySelector('.input-mensagem-erro').innerHtml = ''
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
     } else {
         input.parentElement.classList.add('input-container--invalido')
-        input.parentElement.querySelector('.input-mensagem-erro').innerHtml = mostraMensagemDeErro(tipoDeInput, input)
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
     }
 }
 
@@ -23,25 +23,30 @@ const tiposDeErro = [
 
 const mensagensDeErro = { 
     nome: {
-        valueMissing: 'O campo nome não pode estar vazio'
+        valueMissing: 'O campo nome não pode estar vazio.'
     },
     email: {
-        valueMissing: 'O campo de email não pode estar vazio',
-        typeMismatch: 'O email digitado não é valido'
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é valido.'
     },
     senha: {
-        valueMissing: 'O campo de senha não pode estar vazio',
+        valueMissing: 'O campo de senha não pode estar vazio.',
         patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
     },
     dataNascimento: {
-        valueMissing: 'O campo de data de nascimento não pode estar vazio',
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
         customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        customError: 'O CPF digitado não é válido.'
     }
 
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input) // Chama a função checaCPFRepetido(cpf)
 }
 
 /* const dataNascimento = document.querySelector('#nascimento')
@@ -79,3 +84,44 @@ function maiorQue18(data) {
 
     return dataMais18 <= dataAtual
 }
+
+//Essa função serve para formatar o cpf antes de valida-lo
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if(!checaCPFRepetido(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido.'
+    }
+    
+    input.setCustomValidity(mensagem)
+}
+
+//Essa função verifica se foi digitado numeros repetidos no campo CPF (*em um número de CPF não pode conter números repetidos)
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+
+    ]
+    let cpfValido = true
+
+    valoresRepetidos.forEach( valor => {
+        if(valor == cpf) {
+            cpfValido = false
+        }
+    })
+
+    return cpfValido
+}
+
+let soma = 
+
